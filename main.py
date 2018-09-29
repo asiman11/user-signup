a9c0bf6b8da4574f02bd4a97c144225992c0df92
Andres S <asiman1@gmail.com>
2018-09-29 13:16:59 -0500

    front end

--- main.py
+++ main.py
@@ -4,8 +4,19 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    return render_template('index.html', title="Virtual Lobby :: Login")
     
-    
-    return render_template('index.html', title="Welcome to user-signup")
-    
+@app.route('/signup')
+def signup():
+    return render_template('signup.html', title="Virtual Lobby :: Signup")
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title="Virtual Lobby :: About")
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', title="Virtual Lobby :: Contact")
 app.run(debug = True)
